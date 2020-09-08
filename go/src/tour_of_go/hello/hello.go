dcfa54a81920c31eb4c48ad6c4dd5eba977a3030
Roberto Gianassi <roberto@develer.com>
2020-09-08 16:43:30 +0200

    Add flow_control.go

--- go/src/tour_of_go/hello/hello.go
+++ go/src/tour_of_go/hello/hello.go
@@ -5,7 +5,14 @@ import (
 	"math/rand"
 )
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
 	fmt.Println("Hello, 世界")
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
