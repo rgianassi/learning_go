dcabfe2efbd1e8d966be7d63ea15e83f973c468b
Roberto Gianassi <roberto@develer.com>
2020-09-11 15:59:50 +0200

    Rename main file to main.go

--- go/src/url_shortener/main.go
+++ go/src/url_shortener/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	cache := URLShortener{
+		port: 9090,
+
+		expanderRoute:   "/",
+		shortenRoute:    "/shorten/",
+		statisticsRoute: "/statistics",
+
+		mappings: make(map[string]string),
+
+		statistics: StatsJSON{},
+	}
+
+	http.HandleFunc(cache.shortenRoute, cache.shortenHandler)
+	http.HandleFunc(cache.statisticsRoute, cache.statisticsHandler)
+	http.HandleFunc(cache.expanderRoute, cache.expanderHandler)
+
+	listenAddress := fmt.Sprintf(":%v", cache.port)
+
+	log.Fatal(http.ListenAndServe(listenAddress, nil))
+}
