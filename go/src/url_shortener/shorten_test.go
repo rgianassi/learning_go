380d643682a5777c52285088cdc38e39460378d5
Roberto Gianassi <roberto@develer.com>
2020-09-14 13:06:40 +0200

    Add shorten unit tests

---
+++ go/src/url_shortener/shorten_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestShortenURL(t *testing.T) {
+	tests := []struct {
+		url                string
+		expectedShortenURL string
+	}{
+		{"", "da39a3"},
+		{"https:/github.com/develersrl/powersoft-hmi", "f63377"},
+	}
+
+	for _, test := range tests {
+		actualShortenURL := shorten(test.url)
+		if actualShortenURL != test.expectedShortenURL {
+			t.Errorf("Shorten URL was incorrect, got: %s, wanted: %s.", actualShortenURL, test.expectedShortenURL)
+		}
+	}
+}
