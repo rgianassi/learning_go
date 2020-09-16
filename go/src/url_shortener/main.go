d851e352c9608c1261f098b8e7b0f3ea18eb75c9
Roberto Gianassi <roberto@develer.com>
2020-09-16 13:37:25 +0200

    Refactor main code

--- go/src/url_shortener/main.go
+++ go/src/url_shortener/main.go
@@ -17,7 +17,7 @@ var (
 	persistence = flag.String("load", "persistence.json", "persistence JSON file for URLs")
 )
 
-func unpersistFrom(cache *URLShortener) {
+func unpersist(cache *URLShortener) {
 	log.Println("loading persistence data from:", *persistence)
 
 	f, err := os.Open(*persistence)
@@ -31,7 +31,7 @@ func unpersistFrom(cache *URLShortener) {
 	cache.unpersistFrom(reader)
 }
 
-func persistTo(cache *URLShortener) {
+func persist(cache *URLShortener) {
 	log.Println("storing persistence data to:", *persistence)
 
 	f, err := os.Open(*persistence)
@@ -45,8 +45,42 @@ func persistTo(cache *URLShortener) {
 	cache.persistTo(writer)
 }
 
+func setupHandlerFunctions(cache *URLShortener) {
+	http.HandleFunc(cache.shortenRoute, cache.shortenHandler)
+	http.HandleFunc(cache.statisticsRoute, cache.statisticsHandler)
+	http.HandleFunc(cache.expanderRoute, cache.expanderHandler)
+}
+
+func setupHTTPServerShutdown(cache *URLShortener, server *http.Server, idleConnectionsClosed chan struct{}) {
+	signalChannel := make(chan os.Signal, 1)
+
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGKILL)
+
+	<-signalChannel
+
+	log.Println("shutting down...")
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Printf("HTTP server Shutdown error: %v", err)
+	}
+
+	persist(cache)
+
+	close(idleConnectionsClosed)
+}
+
+func launchHTTPServer(server *http.Server) {
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe error: %v", err)
+	}
+}
+
 func main() {
 	flag.Parse()
+
+	idleConnectionsClosed := make(chan struct{})
+
+	var server http.Server
+	server.Addr = fmt.Sprintf("%s", *address)
 
 	cache := URLShortener{
 		expanderRoute:   "/",
@@ -58,36 +92,12 @@ func main() {
 		statistics: NewStatsJSON(),
 	}
 
-	http.HandleFunc(cache.shortenRoute, cache.shortenHandler)
-	http.HandleFunc(cache.statisticsRoute, cache.statisticsHandler)
-	http.HandleFunc(cache.expanderRoute, cache.expanderHandler)
+	setupHandlerFunctions(&cache)
+	unpersist(&cache)
 
-	unpersistFrom(&cache)
-	var server http.Server
+	go setupHTTPServerShutdown(&cache, &server, idleConnectionsClosed)
 
-	idleConnectionsClosed := make(chan struct{})
-	go func() {
-		signalChannel := make(chan os.Signal, 1)
-		signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGKILL)
-		<-signalChannel
-
-		// We received an interrupt signal, shut down.
-		log.Println("shutting down...")
-		if err := server.Shutdown(context.Background()); err != nil {
-			// Error from closing listeners, or context timeout:
-			log.Printf("HTTP server Shutdown: %v", err)
-		}
-
-		persistTo(&cache)
-
-		close(idleConnectionsClosed)
-	}()
-
-	server.Addr = fmt.Sprintf("%s", *address)
-	if err := server.ListenAndServe(); err != http.ErrServerClosed {
-		// Error starting or closing listener:
-		log.Fatalf("HTTP server ListenAndServe: %v", err)
-	}
+	launchHTTPServer(&server)
 
 	<-idleConnectionsClosed
 	log.Println("shutdown completed")
