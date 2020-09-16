b4c984c6fff46da46be567b83fbe5246abbef0ba
Roberto Gianassi <roberto@develer.com>
2020-09-16 09:52:39 +0200

    Add graceful shutdown

--- go/src/url_shortener/main.go
+++ go/src/url_shortener/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var (
@@ -29,13 +33,36 @@ func main() {
 	http.HandleFunc(cache.statisticsRoute, cache.statisticsHandler)
 	http.HandleFunc(cache.expanderRoute, cache.expanderHandler)
 
-	listenAddress := fmt.Sprintf("%s", *address)
+	log.Println("loading persistence data from:", *persistence)
+	cache.loadPersistenceFile(*persistence)
 
-	log.Fatal(http.ListenAndServe(listenAddress, nil))
+	var server http.Server
+
+	idleConnectionsClosed := make(chan struct{})
+	go func() {
+		signalChannel := make(chan os.Signal, 1)
+		signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGKILL)
+		<-signalChannel
+
+		// We received an interrupt signal, shut down.
+		log.Println("shutting down...")
+		if err := server.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+
+		log.Println("storing persistence data to:", *persistence)
+		cache.storePersistenceFile(*persistence)
+
+		close(idleConnectionsClosed)
+	}()
+
+	server.Addr = fmt.Sprintf("%s", *address)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		// Error starting or closing listener:
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnectionsClosed
+	log.Println("shutdown completed")
 }
-
-/**
-Implement server graceful shutdown. The server should handle SIGINT and SIGKILL
- by saving all its URLs in a file in the current directory, this file should have
-  the same format that the one the -load option accepts.
-*/
