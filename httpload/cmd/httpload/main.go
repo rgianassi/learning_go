fb5bfdf31fcc4613e6a2ca2b5a3ff2820933c1e6
rgianassi <roberto.gianassi+github.com@gmail.com>
2020-09-29 11:51:18 +0200

    Add workers

--- httpload/cmd/httpload/main.go
+++ httpload/cmd/httpload/main.go
@@ -165,6 +165,50 @@ func (lt *LoadTester) genLoadRequest(ctx context.Context, in <-chan string) (<-c
 	return out, errc, nil
 }
 
+func (lt *LoadTester) genLoadRequests(ctx context.Context, config Config, in <-chan string) (<-chan Result, <-chan error, error) {
+	out := make(chan Result)
+	errc := make(chan error, 1)
+
+	var wg sync.WaitGroup
+	numWorkers := config.nWorkers
+	wg.Add(numWorkers)
+
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			defer wg.Done()
+			resultc, errcr, err := lt.genLoadRequest(ctx, in)
+			if err != nil {
+				errc <- err
+				return
+			}
+
+			for result := range resultc {
+				select {
+				case out <- result:
+				case <-ctx.Done():
+					return
+				}
+			}
+			for err := range errcr {
+				select {
+				case errc <- err:
+				case <-ctx.Done():
+					return
+				}
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+
+		close(out)
+		close(errc)
+	}()
+
+	return out, errc, nil
+}
+
 func (lt *LoadTester) genResults(ctx context.Context, in <-chan Result) (<-chan error, error) {
 	errc := make(chan error, 1) // signals goroutine completion
 
@@ -240,7 +284,7 @@ func (lt *LoadTester) run(config Config) error {
 	}
 	errcList = append(errcList, errc)
 
-	resultc, errc, err := lt.genLoadRequest(ctx, urlc)
+	resultc, errc, err := lt.genLoadRequests(ctx, config, urlc)
 	if err != nil {
 		return err
 	}
@@ -354,80 +398,3 @@ func main() {
 
 	os.Exit(exitCodeOk)
 }
-
-/*
-
-## description
-
-Write a load tester for web servers, that is, a program that simulates
-some load for programs that responds to HTTP requests.
-
-## options:
-
-The program should have the following command-line interface and accept
-the following options:
-
-```
-Usage: httpload [options...] URL
-
-Options:
-
--w int        number of workers to run concurrently. default:50.
--n int        number of requests to run. default:200.
--z string   duration of application to send requests. default:unlimited.
-```
-
-Where:
-
-- `URL` is the URL of the server to load-test
-- the total number of requests cannot be smaller than the number of
-concurrent workers
-- If `-z` is given then `-n` is ignored.
-- if `-z` is given then the application stops and exits after the
-specified duration.
-- example of valid duration: `-z 10m` , `-z 3s`
-
-## final report
-
-When the program exits (either by itself if `-z` was given) or
-in response of a CTRL-C, the program should dump on standard output
-the following information:
-
-A summary about request lengths:
-
-```
-Summary:
-
-  Total:        0.0326 secs
-  Slowest:      0.0295 secs
-  Fastest:      0.0003 secs
-  Average:      0.0054 secs
-  Requests/sec: 6132.8565
-```
-
-A distribution of the status codes:
-
-```
-Status code distribution:
-
-  [200] 198 responses
-  [404] 2 responses
-```
-
-## testing
-
-Write one unit test that verifies that the program respects the specified
-number of requests (`-n`)
-
-## workflow
-
-It's ok and advised to split the work into multiple atomic, self-contained PR.
-
-## hints:
-
-- https://golang.org/pkg/time/#ParseDuration
-- https://blog.golang.org/pipelines
-- https://blog.golang.org/context
-- https://golang.org/pkg/net/http/httptest/#NewServer for testing
-
-*/
